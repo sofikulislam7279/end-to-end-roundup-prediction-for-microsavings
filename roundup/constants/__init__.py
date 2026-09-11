bd201b89b62d3b702c625bb9d0c18d7c42ec7ede
sofikulislam7279 <sofikulislam7279@gmail.com>
2026-09-11 21:45:20 +0600

    Data Transformation Component added

--- roundup/constants/__init__.py
+++ roundup/constants/__init__.py
@@ -47,3 +47,8 @@ DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 DATA_VALIDATION_DIR_NAME: str = "data_validation"
 DATA_VALIDATION_DRIFT_REPORT_DIR: str = "drift_report"
 DATA_VALIDATION_DRIFT_REPORT_FILE_NAME: str = "report.yaml"
+
+# Data Transformation Configuration
+DATA_TRANSFORMATION_DIR_NAME: str = "data_transformation"
+DATA_TRANSFORMATION_TRANSFORMED_DATA_DIR: str = "transformed"
+DATA_TRANSFORMATION_TRANSFORMED_OBJECT_DIR: str = "transformed_object"
